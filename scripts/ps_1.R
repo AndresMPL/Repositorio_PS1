b20f31d070e66efd7c8837c1bb061e94f451c667
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-02 14:42:34 -0500

    Update ps_1.R

--- scripts/ps_1.R
+++ scripts/ps_1.R
@@ -26,7 +26,19 @@ tabla0 <- import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_s
 tabla0_tb <- as_tibble(tabla0)
 
 str(tabla0)
-str(tabla0_tb)     #ensayos
+str(tabla0_tb) #ensayos
+
+#data1  https://ignaciomsarmiento.github.io/GEIH2018_sample/page1.html
+#data2  https://ignaciomsarmiento.github.io/GEIH2018_sample/page2.html
+#data3  https://ignaciomsarmiento.github.io/GEIH2018_sample/page3.html
+#data4  https://ignaciomsarmiento.github.io/GEIH2018_sample/page4.html
+#data5  https://ignaciomsarmiento.github.io/GEIH2018_sample/page5.html
+#data6  https://ignaciomsarmiento.github.io/GEIH2018_sample/page6.html
+#data7  https://ignaciomsarmiento.github.io/GEIH2018_sample/page7.html
+#data8  https://ignaciomsarmiento.github.io/GEIH2018_sample/page8.html
+#data9  https://ignaciomsarmiento.github.io/GEIH2018_sample/page9.html
+#data10 https://ignaciomsarmiento.github.io/GEIH2018_sample/page10.html
+
 
 
 
