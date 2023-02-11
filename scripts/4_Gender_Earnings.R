ebb2a23bf105858b752bdb637c04621dd59288f0
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-11 16:55:56 -0500

    Update 4_Gender_Earnings.R
    Ajustes en FWL y actualizo aquí el boostrap para IC

--- scripts/4_Gender_Earnings.R
+++ scripts/4_Gender_Earnings.R
@@ -3,7 +3,7 @@
 #
 #                       4 - Brecha salarial por género 
 #
-#    Salario = Bo + B1*Edad + B2*Mujer + B3*EducNivel + B4*PuestoTrabajo
+#            Ingresos_laborales = Bo + B1*Edad + B2*Mujer + *Controles*
 #
 #------------------------------------------------------------------------------#
 
@@ -24,19 +24,14 @@ library(pacman)
   library(dplyr)
   
 
-
 #Cargamos la base de datos que vamos a usar
 
- 
   tps1_female <- dt_final
-
-#Seleccionamos y validamos las variables que necesitamos
-
 
 
 #Especificamos variables que usaremos y sus interacciones
 
-    y1 <- tps1_female$salario
+    y1 <- tps1_female$Ingresos_laborales
     x1 <- tps1_female$age
     x2 <- tps1_female$female
     x3 <- tps1_female$maxEducLevel
@@ -47,56 +42,123 @@ library(pacman)
     x8 <- tps1_female$college
 
 
-# Perfil Edad-Salario--------------------------------------------------------
+# Perfil Edad-Ingresos_laborales--------------------------------------------------------
   
-  grafico1 <- ggplot(data=tps1_female,
-              mapping = aes(x=age , y = salario, group=as.factor(sex) , color=as.factor(sex))) +
+    
+  #perfil edad-salario-género con observaciones
+
+  perfil_3a <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
               geom_point() + 
-              stat_smooth(method = lm, se = FALSE, level=0.95) + 
+              stat_smooth(method = lm,se = TRUE, level=0.95) + 
+              labs(title = 'Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
               theme_bw()
-  
-  grafico1
-  
-  reg_line <- lm(salario~age,   data = tps1_female)
+    
+     
+  perfil_3b <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
+              geom_point() + 
+              stat_smooth(method = lm,formula= y ~ poly(x, 2), se = TRUE, level=0.95) + 
+              labs(title = 'Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
+              theme_bw()
+
+    
+  #perfil edad-salario-género sin observaciones
+    
+  perfil_4 <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
+              stat_smooth(method = lm,formula= y ~ poly(x, 2), se = TRUE, level=0.95) + 
+              labs(title = 'Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
+              theme_bw()
+    
+
+  reg_line <- lm(Ingresos_laborales~age,   data = tps1_female)
   stargazer(reg_line, type = "text", digits=7, 
-                      title="Perfil Salario ~ Edad", 
-                      column.labels= "Modelo 1", 
+                      title="Perfil Ingresos_laborales ~ Edad", 
+                      column.labels= "Modelo Perfil", 
                       covariate.labels = "Edad",
-                      dep.var.labels = "Ln(Salario)")
+                      dep.var.labels = "Ln(Ingresos Lab.)")
   
   tps1_female$yhat_line= predict(reg_line)
   
-  reg_sq <- lm(salario~age + I(age^2),   data = tps1_female)
-  stargazer(reg_sq, type= "text", digits=7, title="Perfil Salario ~ Edad + Edad^2")
+  reg_sq <- lm(Ingresos_laborales~age + I(age^2),   data = tps1_female)
+  stargazer(reg_sq, type= "text", digits=7, 
+            title="Perfil Ingresos_laborales ~ Edad + Edad^2",
+            column.labels = "Modelo Perfil",
+            covariate.labels = "Edad",
+            dep.var.labels = "Ln(Ingresos Lab.)")
+  
   tps1_female$yhat_sq= predict(reg_sq)
   
-  perfil = tps1_female %>%
-                        group_by(age) %>%
-                        summarize(mean_y = mean(salario),
-                                  yhat_line = mean(yhat_line),
-                                  yhat_sq=mean(yhat_sq),
-                                  .groups="drop")
-  
-  grafico2 <- ggplot(perfil) + 
-              geom_point(aes(x = age, y = mean_y), color = "#FF4500", size = 2) + 
+  perfil_promedio = tps1_female %>%
+                    group_by(age, sex) %>%
+                    summarize(mean_y = mean(Ingresos_laborales),
+                    yhat_line = mean(yhat_line),
+                    yhat_sq=mean(yhat_sq),
+                    .groups="drop")
+          
+  perfil_5 <- ggplot(perfil_promedio) + 
+              geom_point(aes(x = age, y = mean_y, group=as.factor(sex)), color = "#FF4500", size = 2) + 
               geom_line(aes(x = age, y = yhat_line), color = "#0000EE", size = 1) +
               geom_line(aes(x = age, y = yhat_sq), color = "#00CD00", size = 1) +
               labs(title = "Perfil Edad-Salario", x = "Edad", y = "Salario") +
               theme_bw()
-
-grafico2 #revisar atípicos
+  
+  perfil_5 #revisar atípicos
 
 
 #Edades pico con intervalos de confianza----------------------------
 
-
+  
+  reg_intervalo_conf <- function(x, y) {
+    n <- length(y) # Find length of y to use as sample size
+    lm_model <- lm(y ~ x + I(x^2)) # Fit linear model 
+    
+    # Extract fitted coefficients from model object
+    b0 <- lm_model$coefficients[1]
+    b1 <- lm_model$coefficients[2]
+    b2 <- lm_model$coefficients[3]
+    
+    # Find SSE and MSE
+    sse <- sum((y - lm_model$fitted.values)^2)
+    mse <- sse / (n - 2)
+    
+    t_val <- qt(0.975, n - 2) # Calculate critical t-value
+    
+    # Fit linear model with extracted coefficients
+    x_new <- 1:max(x)
+    y_fit <- b0 + b1*x_new + b2*x_new^2
+    
+    # Find the standard error of the regression line
+    se <- sqrt(sum((y - y_fit)^2) / (n - 2)) * sqrt(1 / n + (x - mean(x))^2 / sum((x - mean(x))^2))
+    
+    # Fit a new linear model that extends past the given data points (for plotting)
+    x_new2 <- 1:max(x + 1000)
+    y_fit2 <- b0 + b1*x_new2 + b2*x_new2^2
+    
+    # Warnings of mismatched lengths are suppressed
+    slope.upper <- suppressWarnings(y_fit2 + t_val * se)
+    slope.lower <- suppressWarnings(y_fit2 - t_val * se)
+    
+    # Collect the computed confidence bands into a data.frame and name the colums
+    bands <- data.frame(cbind(slope.lower , slope.upper))
+    colnames(bands) <- c('Lower Confidence Band', 'Upper Confidence Band')
+    
+    # Plot the fitted linear regression line and the computed confidence bands
+    plot(x, y, cex = 1, pch = 21, bg = 'gray')
+    lines(y_fit2, col = 'black', lwd = 2)
+    lines(bands[1], col = 'blue', lty = 2, lwd = 2)
+    lines(bands[2], col = 'blue', lty = 2, lwd = 2)
+    
+    return(bands)
+    
+  }
+  
+  intervalo_conf <- reg_intervalo_conf(tps1_female$age, tps1_female$salario)
 
 # FWL --------------------------------------------------------------------------
 
 #Incrementamos la variable control para hacer mas clara la diferencia
 
-  #summary(tps1_female$salario) #voy a incrementar female=1 
-  #tps1_female <- tps1_female %>% mutate(salario=ifelse(female==1,salario+1000,salario))
+  #summary(tps1_female$Ingresos_laborales) #voy a incrementar female=1 
+  #tps1_female <- tps1_female %>% mutate(Ingresos_laborales=ifelse(female==1,Ingresos_laborales+1000,Ingresos_laborales))
 
 # unconditional wage gap 
 
@@ -146,16 +208,16 @@ tps1_female <- tps1_female %>%
   grafico3 <- ggplot(tps1_female,aes(y=Ingresos_laborales,x=age,group=female,col=factor(female))) +
               geom_point() +
               geom_smooth(method = lm, se = FALSE) +
-              labs(title = "Perfil Edad-Salario-Género", x = "Edad", y = "Salario") +
+              labs(title = "Perfil Edad-Ingresos_laborales-Género", x = "Edad", y = "Ingresos_laborales") +
               theme_bw()
   
   grafico3
 
-  #La línea azul muestra la regresión del salario solo con la edad
-  #Las otras dos líneas muestran el salario con la edad, separando el género.
+  #La línea azul muestra la regresión del Ingresos_laborales solo con la edad
+  #Las otras dos líneas muestran el Ingresos_laborales con la edad, separando el género.
   #La línea rosada - hombres
   #La línea celeste - mujeres
-  #El salario si está influido por el género, cuando es hombre aumenta más que cuando son mujeres
+  #El Ingresos_laborales si está influido por el género, cuando es hombre aumenta más que cuando son mujeres
   #Por eso cuando incluimos la línea de las mujeres la regresión azul cae
 
 
