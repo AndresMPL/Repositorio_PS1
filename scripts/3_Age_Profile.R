e85e334903270365ff336f074c32d25d80388d89
IsabellaMendez <124231477+IsabellaMendez@users.noreply.github.com>
2023-02-11 22:26:09 -0500

    Update 3_Age_Profile.R

--- scripts/3_Age_Profile.R
+++ scripts/3_Age_Profile.R
@@ -4,8 +4,9 @@
 #              3. Age-Earning Profile
 #----------------------------------------------------
 
+dt_edad<- dt_final %>% select(age,college, estrato1, ingtot,experiencia, y_total_m_ha, Ingresos_laborales, age_squred, female)
 
-reg_wage <- lm(dt_final$Ingresos_laborales ~ dt_final$age + I(dt_final$age_squred))
+reg_wage <- lm(dt_edad$Ingresos_laborales ~ dt_edad$age + I(dt_edad$age_squred))
 stargazer(reg_wage, type="latex")
 reg_wage
 
@@ -13,13 +14,13 @@ reg_wage
 #Intervalos de confianza
 
 boot1 <- function(data, index){
-  coef(lm(Ingresos_laborales ~ age + I(age_squred), data = dt_final, subset = index))}
+  coef(lm(Ingresos_laborales ~ age + I(age_squred), data = dt_edad, subset = index))}
 
-boot2 <- boot(dt_final, boot1, R=1000)
+boot2 <- boot(dt_edad, boot1, R=1000)
 boot3 <- t(rbind(boot2$t0, apply(boot2$t, 2, function(x) sd(x))))
 
 coeffunct <- function(data, index){
-  reg_age <- lm(Ingresos_laborales ~ age + I(age_squred), data = dt_final, subset = index)
+  reg_age <- lm(Ingresos_laborales ~ age + I(age_squred), data = dt_edad, subset = index)
   coefs <- reg_age$coefficients
   b2 <- coefs[2]
   b3 <- coefs[3]
@@ -27,7 +28,7 @@ coeffunct <- function(data, index){
   return(peak_age)
 }
 
-boot4 <- boot(dt_final, coeffunct, R=1000)
+boot4 <- boot(dt_edad, coeffunct, R=1000)
 boot5 <- t(rbind(boot4$t0, apply(boot4$t, 2, function(x) sd(x))))
 
 alpha <- 0.05
@@ -39,16 +40,16 @@ b12 <- boot2$t0[2]
 b13 <- boot2$t0[3]
 
 
-resul_age <- lm(Ingresos_laborales ~ age + I(age_squred), data=dt_final)
+resul_age <- lm(Ingresos_laborales ~ age + I(age_squred), data=dt_edad)
 summary(resul_age)
 
 
 maximo <- resul_age$fitted.values
 peak <- -(resul_age$coefficients[2])/(2*(resul_age$coefficients[3]))
-dt_final$p_edad <- resul_age$coefficients[1]+(resul_age$coefficients[2]*dt_final$age)+(resul_age$coefficients[3]*I(dt_final$age_squred))
+dt_edad$p_edad <- resul_age$coefficients[1]+(resul_age$coefficients[2]*dt_edad$age)+(resul_age$coefficients[3]*I(dt_edad$age_squred))
 
 
-ggplot(dt_final, aes(x=age, y=p_edad)) + 
+ggplot(dt_edad, aes(x=age, y=p_edad)) + 
   geom_point(color='#424242', size=0.9) +
   geom_vline(aes(xintercept=peak), linetype='dashed', size=1, color='darkblue')+
   geom_linerange(aes(xmin=min, xmax=max, y = max(maximo)), size=2, color='coral1')+ xlab('Edad') +  ylab('Log ingresos totales') +
