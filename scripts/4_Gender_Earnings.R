20963870b4d70f96cda41c6cb98883fb7837645c
Andres Palacio <am.palaciol@uniandes.edu.co>
2023-02-12 19:27:41 -0500

    Update 4_Gender_Earnings.R

--- scripts/4_Gender_Earnings.R
+++ scripts/4_Gender_Earnings.R
@@ -32,56 +32,12 @@ library(pacman)
 
 # Perfil Edad-Ingresos_laborales--------------------------------------------------------
 
-  #perfil edad-salario-género con observaciones
-
-  perfil_3a <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
-              geom_point() + 
-              stat_smooth(method = lm,se = TRUE, level=0.95) + 
-              labs(title = 'Figura 5. Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
-              theme_bw()
-    
-     
-  perfil_3b <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
-              geom_point() + 
-              stat_smooth(method = lm,formula= y ~ poly(x, 2), se = TRUE, level=0.95) + 
-              labs(title = 'Figura 6. Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
-              theme_bw()
-
-    
-  #perfil edad-salario-género sin observaciones
-
-              max_mujer <- dt_final %>% subset(sex == 1)
-              reg_mujer <- lm(Ingresos_laborales~age + I(age^2),   data = max_mujer)
-              max_mujer$yhat_sq= predict(reg_mujer)
-              maxm <- max_mujer %>% group_by(age, sex) %>%
-                                summarize(mean_ysq = mean(yhat_sq))
-              
-              maxm1 <- maxm %>% filter(mean_ysq==max(maxm$mean_ysq, na.rm=TRUE))
-              
-              max_hombre <- dt_final %>% subset(sex == 0)
-              reg_jombre <- lm(Ingresos_laborales~age + I(age^2),   data = max_hombre)
-              max_hombre$yhat_sq= predict(reg_jombre)
-              maxh <- max_hombre %>% group_by(age, sex) %>%
-                summarize(mean_ysq = mean(yhat_sq))
-              
-              maxh1 <- maxh %>% filter(mean_ysq==max(maxh$mean_ysq, na.rm=TRUE))
-  
-   perfil_4 <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
-              geom_vline(xintercept = maxm1$age, linetype = 5, size=0.7, col="#008B8B")+
-              geom_vline(xintercept = maxh1$age, linetype = 5,  size=0.7, col="#CD5B45")+ 
-              geom_hline(yintercept = maxm1$mean_ysq, linetype = 5, size=0.7, col="#008B8B")+
-              geom_hline(yintercept = maxh1$mean_ysq, linetype = 5,  size=0.7, col="#CD5B45")+
-              stat_smooth(method = lm,formula= y ~ poly(x, 2), se = TRUE, level=0.95) + 
-              labs(title = 'Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
-              theme_bw()
-    
-
   reg_line <- lm(Ingresos_laborales~age,   data = tps1_female)
   stargazer(reg_line, type = "text", digits=7, 
-                      title="Perfil Ingresos laborales ~ Edad y Género", 
-                      column.labels= "Modelo Perfil", 
-                      covariate.labels = "Edad",
-                      dep.var.labels = "Ln(Ingresos Lab.)")
+            title="Perfil Ingresos laborales ~ Edad y Género", 
+            column.labels= "Modelo Perfil", 
+            covariate.labels = "Edad",
+            dep.var.labels = "Ln(Ingresos Lab.)")
   
   tps1_female$yhat_line= predict(reg_line)
   
@@ -97,18 +53,55 @@ library(pacman)
   perfil_promedio = tps1_female %>%
                     group_by(age, sex) %>%
                     summarize(mean_y = mean(Ingresos_laborales),
-                    yhat_line = mean(yhat_line),
-                    yhat_sq=mean(yhat_sq),
-                    .groups="drop")
+                              yhat_line = mean(yhat_line),
+                              yhat_sq=mean(yhat_sq),
+                              .groups="drop")
   
   perfil_5 <- ggplot(perfil_promedio) + 
               geom_point(aes(x = age, y = mean_y, group=as.factor(sex)), color = "#FF4500", size = 2) + 
               geom_line(aes(x = age, y = yhat_line), color = "#0000EE", size = 1) +
               geom_line(aes(x = age, y = yhat_sq), color = "#00CD00", size = 1) +
-              labs(title = "Perfil Ingresos laborales ~ Edad y Género", x = "Edad", y = "Salario") +
+              labs(title = "Figura 6. Perfil Ingresos laborales ~ Edad", x = "Edad", y = "Salario") +
               theme_bw()
   
-  perfil_5 #revisar atípicos
+  perfil_5
+  
+  #perfil edad-salario-género con observaciones
+
+  perfil_3 <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
+              geom_point() + 
+              stat_smooth(method = lm,formula= y ~ poly(x, 2), se = TRUE, level=0.95) + 
+              labs(title = 'Figura 7. Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
+              theme_bw()
+
+  perfil_3
+  
+  #perfil edad-salario-género - Edades Picos
+
+      max_mujer <- dt_final %>% subset(sex == 1)
+      reg_mujer <- lm(Ingresos_laborales~age + I(age^2),   data = max_mujer)
+      max_mujer$yhat_sq= predict(reg_mujer)
+      maxm <- max_mujer %>% group_by(age, sex) %>%
+                            summarize(mean_ysq = mean(yhat_sq))
+      maxm1 <- maxm %>% filter(mean_ysq==max(maxm$mean_ysq, na.rm=TRUE))
+
+      max_hombre <- dt_final %>% subset(sex == 0)
+      reg_jombre <- lm(Ingresos_laborales~age + I(age^2),   data = max_hombre)
+      max_hombre$yhat_sq= predict(reg_jombre)
+      maxh <- max_hombre %>% group_by(age, sex) %>%
+                             summarize(mean_ysq = mean(yhat_sq))
+      maxh1 <- maxh %>% filter(mean_ysq==max(maxh$mean_ysq, na.rm=TRUE))
+  
+  perfil_4 <- ggplot(data=dt_final, mapping = aes(x=age , y = Ingresos_laborales, group=as.factor(sex) , color=as.factor(sex))) +
+              geom_vline(xintercept = maxm1$age, linetype = 5, size=0.7, col="#008B8B")+
+              geom_vline(xintercept = maxh1$age, linetype = 5,  size=0.7, col="#CD5B45")+ 
+              geom_hline(yintercept = maxm1$mean_ysq, linetype = 5, size=0.7, col="#008B8B")+
+              geom_hline(yintercept = maxh1$mean_ysq, linetype = 5,  size=0.7, col="#CD5B45")+
+              stat_smooth(method = lm,formula= y ~ poly(x, 2), se = TRUE, level=0.95) + 
+              labs(title = 'Figura 8. Perfil Edad vs. Salario por Género', x = 'Edad', y = 'Salarios') + 
+              theme_bw()
+
+  perfil_4
 
 
 #Edades pico con intervalos de confianza----------------------------
@@ -154,8 +147,7 @@ library(pacman)
     lines(bands[2], col = '#FF7F00', lty = 3, lwd = 2)
     
     return(bands)
-    
-  }
+    }
   
   intervalo_conf <- reg_intervalo_conf(tps1_female$age, tps1_female$Ingresos_laborales)
 
@@ -165,6 +157,7 @@ library(pacman)
 
   reg_unconditional <- lm(Ingresos_laborales ~ female , data = tps1_female)
   stargazer(reg_unconditional, type= "text", digits=7, title="unconditional wage gap")
+  
 
 #Conditional Model
 
@@ -206,9 +199,9 @@ library(pacman)
   sqrt(diag(vcov(reg1)))[2]
 
 
-#Gráfico
+#Gráfico. Aquí validamos que si omitimos un regresor se cambia los coeficientes de otros regresores.
   
-  reg3 <- lm(Ingresos_laborales ~ age, tps1_fwl)   #Si omitimos un regresor se cambia los coeficientes de otros regresores
+  reg3 <- lm(Ingresos_laborales ~ age, tps1_fwl)  
   
   grafico3 <- ggplot(tps1_fwl,aes(y=Ingresos_laborales,x=age,group=female,col=factor(female))) +
               geom_point() +
@@ -216,24 +209,17 @@ library(pacman)
               geom_abline(slope=reg3$coefficients[2],	#pendiente - aquí vemos la regresión solo de y1~x1
                           intercept=reg3$coefficients[1], #intercepto
                           color="blue", size=1) +
-              labs(title = "Perfil Ingresos laborales ~ Edad y Género", x = "Edad", y = "Salario") +
+              labs(title = "Figura 9. Perfil Ingresos laborales ~ Edad y Género", x = "Edad", y = "Salario") +
                   theme_bw()
                    
   grafico3
-
-  #La línea azul muestra la regresión del Ingresos_laborales solo con la edad
-  #Las otras dos líneas muestran el Ingresos_laborales con la edad, separando el género.
-  #La línea rosada - hombres
-  #La línea celeste - mujeres
-  #El Ingresos_laborales si está influido por el género, cuando es hombre aumenta más que cuando son mujeres
-  #Por eso cuando incluimos la línea de las mujeres la regresión azul cae
 
 
 #FWL con bootstrap--------------------------------------------------------------
 
   bt_red2 <- function(data,index){
     
-    #take the residuals
+    #Take the residuals
     
     tps1_female <- tps1_female %>% mutate(female_Resid = lm(female ~ age + cuentaPropia + informal + maxEducLevel3 + maxEducLevel4 + maxEducLevel5 + maxEducLevel6 + maxEducLevel7 + 
                                                               microEmpresa + experiencia + Ingresos_laborales + female + age_squred, tps1_female, subset = index)$residuals)
@@ -242,15 +228,15 @@ library(pacman)
                                                                 microEmpresa + experiencia + Ingresos_laborales + female + age_squred, tps1_female, subset = index)$residuals, subset = index)
     
     
-    #  Regress the residuals, get the coefficients
+    #Regress the residuals, get the coefficients
     coefs <- lm(ingresos_Resid~female_Resid, tps1_female, subset = index)$coefficients
     
     b1 <- coefs[2]
     
-    #return coefficients
+    #Return coefficients
     
     return(b1)
-  }
+    }
   
   #test example  
 
